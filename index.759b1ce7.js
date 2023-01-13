c3d1b4ea8d087bd42c8463b780e7688fd07ac4e9
NikitaShvindt <116110595+NikitaShvindt@users.noreply.github.com>
2023-01-13 17:14:04 +0000

    Deploying to gh-pages from @ NikitaShvindt/notsogreen@ab2459b13e5a1bef7daf24fc1422ac46880331f2 🚀

---
+++ index.759b1ce7.js
@@ -0,0 +1,2 @@
+$(".single-item").slick({autoplay:!0});
+//# sourceMappingURL=index.759b1ce7.js.map
---
+++ index.890e494a.js
@@ -0,0 +1,2 @@
+$(".single-item").slick({autoplay:!0});
+//# sourceMappingURL=index.890e494a.js.map
